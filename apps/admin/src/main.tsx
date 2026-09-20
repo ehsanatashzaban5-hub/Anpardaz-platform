8b49022e8e987fe07bf64891e8085af75a70dbae
ehsanatashzaban5-hub <ehsanatashzaban5@gmail.com>
2026-09-20 19:51:38 +0330

    refactor(admin): add standalone admin entrypoint

---
+++ apps/admin/src/main.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import AdminPanel from "./AdminPanel";
+import "./admin.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <AdminPanel />
+  </React.StrictMode>,
+);
